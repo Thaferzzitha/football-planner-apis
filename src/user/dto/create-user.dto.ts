12a21ad4632917ad4eb09b25ba173883d285fb5c
Thaferzzitha <thaferzzitha1@gmail.com>
2024-05-10 17:02:23 -0500

    user model created

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,24 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
+export class CreateUserDto {
+    @ApiProperty({example: 'John'})
+    @IsString()
+    @IsNotEmpty()
+    readonly firstName: string;
+  
+    @ApiProperty({example: 'Doe'})
+    @IsString()
+    @IsNotEmpty()
+    readonly lastName: string;
+  
+    @ApiProperty({example: 'john@mail.com'})
+    @IsNotEmpty()
+    @IsEmail()
+    readonly email: string;
+  
+    @ApiProperty({example: '123456'})
+    @IsString()
+    @IsNotEmpty()
+    password: string;
+}
---
+++ src/user/models/user.model.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import {type HydratedDocument } from "mongoose";
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema({timestamps: true})
+export class User {
+  @Prop({ type: String, unique: true, required: true })
+  email: string;
+
+  @Prop({ type: String, required: true })
+  firstName: string;
+
+  @Prop({ type: String, required: true })
+  lastName: string;
+
+  @Prop({ type: String, required: false, default: '' })
+  profilePicture: string;
+
+  @Prop({ type: String, required: true })
+  password: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
