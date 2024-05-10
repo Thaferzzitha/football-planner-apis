38715379fd39157c2358124fc21815c5e6989e4f
Thaferzzitha <thaferzzitha1@gmail.com>
2024-05-10 16:28:23 -0500

    config mongoose on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>(
+          'MONGO_DB_CONNECTION',
+          'mongodb://root:root@localhost:27017/nest?authSource=admin',
+        ),
+      }),
+      inject: [ConfigService],
+    }),
+    MongooseModule.forFeature([
+      /* { name: Dimension.name, schema: DimensionSchema }, */
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
